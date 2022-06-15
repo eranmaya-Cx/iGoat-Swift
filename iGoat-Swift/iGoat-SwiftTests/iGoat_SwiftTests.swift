4cf7db05dda068c3f9f4b8ca794c82b254c153fa
eranmaya-Cx <93197330+eranmaya-Cx@users.noreply.github.com>
2022-06-15 15:39:44 +0300

    Update iGoat_SwiftTests.swift

--- iGoat-Swift/iGoat-SwiftTests/iGoat_SwiftTests.swift
+++ iGoat-Swift/iGoat-SwiftTests/iGoat_SwiftTests.swift
@@ -9,7 +9,6 @@ class iGoat_SwiftTests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     

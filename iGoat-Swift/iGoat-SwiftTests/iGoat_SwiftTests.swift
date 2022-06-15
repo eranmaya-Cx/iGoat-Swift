9420b932df5af29feb6b923fccac82d32c8348e8
eranmaya-Cx <93197330+eranmaya-Cx@users.noreply.github.com>
2022-06-15 15:37:33 +0300

    Update iGoat_SwiftTests.swift

--- iGoat-Swift/iGoat-SwiftTests/iGoat_SwiftTests.swift
+++ iGoat-Swift/iGoat-SwiftTests/iGoat_SwiftTests.swift
@@ -14,8 +14,7 @@ class iGoat_SwiftTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
     }
     
     func testPerformanceExample() {

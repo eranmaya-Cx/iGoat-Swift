3fa49e07b21c5370fe172382746af7999f590930
eranmaya-Cx <93197330+eranmaya-Cx@users.noreply.github.com>
2022-06-15 11:33:58 +0300

    Update iGoat-Objc-Bridging-Header.h

--- iGoat-Swift/iGoat-Objc-Bridging-Header.h
+++ iGoat-Swift/iGoat-Objc-Bridging-Header.h
@@ -6,7 +6,7 @@
 #endif /* iGoat_Objc_Bridging_Header_h */
 
 
-#import "UIImage+animatedGIF.h"
+#import "UIImage+animatedGIF.h" /* iGoat_Objc_Bridging_Header_h */
 #import "SVProgressHUD.h"
 #import <CommonCrypto/CommonCrypto.h>
 #import <sqlite2.h>

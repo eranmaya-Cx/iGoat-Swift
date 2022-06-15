218e398767cd07109055a32f7a4ad61034130144
eranmaya-Cx <93197330+eranmaya-Cx@users.noreply.github.com>
2022-06-15 15:35:59 +0300

    Update iGoat_SwiftTests.swift

--- iGoat-Swift/iGoat-SwiftTests/iGoat_SwiftTests.swift
+++ iGoat-Swift/iGoat-SwiftTests/iGoat_SwiftTests.swift
@@ -6,7 +6,6 @@ class iGoat_SwiftTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
